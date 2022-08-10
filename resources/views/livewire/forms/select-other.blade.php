4ec9908053530fc3a82a90a7a1b632b491a278f2
Dimitrije Borcanin <dimitrijeborcanin@gmail.com>
2022-08-10 11:30:39 +0200

    Small fixes and routes

--- resources/views/livewire/forms/select-other.blade.php
+++ resources/views/livewire/forms/select-other.blade.php
@@ -1,20 +1,22 @@
 <div>
     <x-jet-label for="{{$inputId}}" value="{{$title}}" />
     @if($isSelect)
-    <select wire:model="value" class="form-input rounded-md shadow-sm block mt-1 w-full py-2" id="{{$inputId}}" wire:change="checkIfOther($event.target.value)">
+    <select wire:model="value" class="form-input rounded-md shadow-sm block mt-1 w-full py-2 pr-5" id="{{$inputId}}" wire:change="checkIfOther($event.target.value)">
         <option value="">Izaberite...</option>
         @foreach($list as $item)
-                <option value="{{$item["value"]}}">{{$item["title"]}}</option>
+                <option value="{{$item["value"]}}" wire:key="{{$item["value"]}}">{{$item["title"]}}</option>
         @endforeach
-        <option value="other">Ostalo</option>
+        <option value="other" wire:key="other">Ostalo</option>
     </select>
     @else
-    <div class="flex">
-        <x-jet-input id="{{$inputId}}" type="text" class="mt-1 grow" wire:model="value" wire:keyup="customInputChange()" />
-        <div class="flex cursor-pointer items-center hover:text-gray-500 ml-3" wire:click="closeCustomInput()">
-            <i class="fas fa-rectangle-xmark text-4xl"></i>
+    <div class="relative">
+        <x-jet-input id="{{$inputId}}" type="text" class="w-full pr-9" wire:model="value" wire:keyup="customInputChange()" />
+        <div class="absolute top-1 right-3">
+            <div class="flex cursor-pointer items-center text-gray-400 hover:text-gray-500 ml-3 items-center z-10" wire:click="closeCustomInput()">
+                <i class="fas fa-xmark text-2xl"></i>
+            </div>
         </div>
     </div>
     @endif
-    <x-jet-input-error for="{{$inputId}}" class="mt-2" />
+    {{-- <x-jet-input-error for="{{$inputId}}" class="mt-2" /> --}}
 </div>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use App\Http\Livewire\Customer\CreateCustomer;
 use App\Http\Livewire\Customer\ShowCustomer;
 use App\Http\Livewire\Customer\ShowCustomers;
+use App\Http\Livewire\Orders\Dashboard;
+use App\Http\Livewire\Orders\ShowOrder;
+use App\Http\Livewire\Product\CreateProduct;
 use App\Http\Livewire\User\ShowUsers;
 use Illuminate\Support\Facades\Route;
 
@@ -19,9 +22,9 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified'])->group(function () {
 
-    Route::get('/', function () {
-        return view('dashboard');
-    })->name('dashboard');
+    // Route::get('/', function () {
+    //     return view('dashboard');
+    // })->name('dashboard');
 
     // Users
     Route::get('/zaposleni', ShowUsers::class)->name('users')->middleware(['roles:1']);
@@ -30,4 +33,11 @@ Route::middleware(['auth:sanctum', config('jetstream.auth_session'), 'verified']
     Route::get('/kupci', ShowCustomers::class)->name('customers')->middleware(['roles:1,2,3']);
     Route::get('/kupci/dodaj', CreateCustomer::class)->name('customers.create')->middleware(['roles:1,2,3']);
     Route::get('/kupci/{customer}', ShowCustomer::class)->name('customer')->middleware(['roles:1,2,3']);
+
+    // Products
+    Route::get('/kupci/{customer}/porudzbina/dodaj', CreateProduct::class)->name('products.create')->middleware(['roles:1,2,3']);
+
+    // Orders
+    Route::get('/', Dashboard::class)->name('dashboard');
+    Route::get('/kupci/{customer}/porudzbina/{order}', ShowOrder::class)->name('order')->middleware(['roles:1,2,3']);
 });
